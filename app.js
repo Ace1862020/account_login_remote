3bbb04ed34f60b59351f59fa4022c4b5e9af3b11
Ace1862020 <kyds1862015@gmail.com>
2021-02-15 19:44:46 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const port = 3000
 const app = express()
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send('Hello world!!')
+  res.render('index')
 })
 
 app.listen(port, () => {
