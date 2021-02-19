6329f437fc37f00093f90802562d6b63349449fd
Ace1862020 <kyds1862015@gmail.com>
2021-02-19 12:05:25 +0800

    feat: add sign in route

--- app.js
+++ app.js
@@ -30,12 +30,21 @@ app.get('/', (req, res) => {
 
 app.post('/member', (req, res) => {
   const memberData = req.body
+  const { email, password } = memberData
+
   Account.find()
     .lean()
     .then((accounts) => {
+      const user = accounts.find((item) => {
+        return (item.email === email) && (item.password === password)
+      })
 
-      console.log('account:', accounts)
-
+      if (user !== undefined) {
+        memberData.firstName = user.firstName
+        return res.render('member', { memberData })
+      } else {
+        return res.render('index', { memberData, message: 'message' })
+      }
     })
     .catch(error => console.log(error))
 })
